94adffd15578becdddbb865d8dd342b96dfd2a78
hanzusd <hannalotta.alenius@helsinki.fi>
2022-10-18 20:45:58 +0300

    ecercise 9.22

--- src/SinglePatientPage/index.tsx
+++ src/SinglePatientPage/index.tsx
@@ -45,8 +45,28 @@ const SinglePatientPage = ( ) => {
         genderIcon = "\u26A5";
     }
 
+    const assertNever = (value: never): never => {
+      throw new Error(
+        `Unhandled discriminated union member: ${JSON.stringify(value)}`
+      );
+    };
+
+    const userHealthIcon = (health: number) => {
+      if (health === 0) {
+        return ("\u{1F49A}");
+      }
+      if (health === 1) {
+        return ("\u{1F49B}");
+      }
+      if (health === 2) {
+        return ("\u{1F9E1}");
+      }
+      if (health === 3) {
+        return ("\u{1F5A4}");
+      }
+    };
+
     const getEntries = (entries:Array<Entry>) => {
-      
       const printDiagnoses = (entry:Entry) => {
         if (entry.diagnosisCodes!==undefined) {
           return (entry.diagnosisCodes.map(d => {
@@ -61,17 +81,39 @@ const SinglePatientPage = ( ) => {
 
       const printEntry = (entries:Array<Entry>) => {
         return(entries.map(e =>
-          <div key={e.id} style={{marginBottom: "1rem"}}>
-            <div >{e.date} <i>{e.description}</i></div>
+          <div key={e.id} style={{marginBottom: "1rem", borderStyle: "solid", padding:"10px"}}>
+            <div>{e.date}</div>
+            <div style = {{fontStyle:"italic"}}>{e.description}</div>
+            <SpecialDiagnoseParts entry = {e} />
             <div style={{marginTop: "1rem", marginLeft: "1rem"}}>{printDiagnoses(e)}</div>
+            <div>diagnose by {e.specialist}</div>
           </div>));
           };
-
 
       return (
       <div>
         {printEntry(entries)}
       </div>);
+    };
+
+    const SpecialDiagnoseParts = (props:{entry:Entry}) => {
+      switch(props.entry.type) {
+        case "Hospital":
+          return (<div>
+            <div>discharge date: {props.entry.discharge.date}</div>
+            <div>criteria for discharge: {props.entry.discharge.criteria}</div>
+          </div>);
+        case "OccupationalHealthcare":
+          return (<div>
+            employer: {props.entry.employerName}
+          </div>);
+        case "HealthCheck":
+          return (<div>
+            <div>{userHealthIcon(props.entry.healthCheckRating)}</div>
+          </div>);
+        default:
+          return assertNever(props.entry);
+      }
     };
 
     return(
@@ -80,7 +122,7 @@ const SinglePatientPage = ( ) => {
             <div>ssn: {patient.ssn}</div>
             <div style={{marginBottom: "1rem"}}>occupation: {patient.occupation}</div>
             <div style={{fontWeight:"bold", fontSize:25}}>entries</div>
-            <div style={{marginTop: "1rem"}}>{getEntries(patient.entries)}</div>
+            <div style={{marginTop: "1rem"}} >{getEntries(patient.entries)}</div>
         </div>);
 };
 
